987ac0c3b32c130b91037eebaececc342c78be20
Bruno Ferreira <seu@email.com>
2024-03-12 21:31:49 -0300

    inclusão das classes model e definição dos relacinamentos

---
+++ src/main/java/brunoferreira/sistemabancario/model/Transacao.java
@@ -0,0 +1,43 @@
+package brunoferreira.sistemabancario.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb-transacao")
+public class Transacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_conta")
+    private Conta conta;
+
+    @Column(nullable = false)
+    private Double valor;
+
+    public Long getIdTransacao() {
+        return idTransacao;
+    }
+
+    public void setIdTransacao(Long idTransacao) {
+        this.idTransacao = idTransacao;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+}
---
+++ src/main/java/brunoferreira/sistemabancario/model/Usuario.java
@@ -0,0 +1,53 @@
+package brunoferreira.sistemabancario.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb-usuarios")
+public class Usuario {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Conta> contas;
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
+}
